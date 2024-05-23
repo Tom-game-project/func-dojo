1636c752b968c3e5c0606c434aab3528bb8d4024
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-05-23 14:48:43 +0900

    remove typing

--- python/tarai.py
+++ python/tarai.py
@@ -1,2 +1,2 @@
-tarai:int = lambda x:lambda y:(lambda _:y) if x <= y else lambda z:tarai(tarai(x-1)(y)(z))(tarai(y-1)(z)(x))(tarai(z-1)(x)(y))
+tarai = lambda x:lambda y:(lambda _:y) if x <= y else lambda z:tarai(tarai(x-1)(y)(z))(tarai(y-1)(z)(x))(tarai(z-1)(x)(y))
 print(tarai(12)(6)(0))
