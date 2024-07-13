85dfb414cf37d4ae975758a8d8e5e2dcfeb0b280
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-07-13 15:10:20 +0900

    rename value

--- javascript/tarai_flat.js
+++ javascript/tarai_flat.js
@@ -2,17 +2,17 @@
 let i = 0;
 // 関数を並列させる実装
 
-function f2_0(local2x,local2y,z){return local2y;}
+function f2_0(x,y,z){return y;}
 
-function f2_1(local2x,local2y,z){
-    let nx = tarai2 (local2x - 1, local2y, z);
-    let ny = tarai2 (local2y - 1, z, local2x);
-    let nz = tarai2 (z - 1, local2x, local2y);
+function f2_1(x,y,z){
+    let nx = tarai2 (x - 1, y, z);
+    let ny = tarai2 (y - 1, z, x);
+    let nz = tarai2 (z - 1, x, y);
     return tarai2(nx,ny,nz);
 }
 
-function f1(local1x,y){
-    if (local1x <= y){
+function f1(x,y){
+    if (x <= y){
         i++;
         return f2_0
     }
